486edeb5b47927d340732548e25cf9b3235a93a4
javay johnson <javayjohnson@gmail.com>
2024-08-18 20:24:09 -0400

    commit 4 working console log for first round

--- app.js
+++ app.js
@@ -1,74 +1,96 @@
+
+// This console log works!
+
 class CaptainChakaKhan {
     constructor() {
-      this.hull = 20;
-      this.firepower = 5
-      this.accuracy = 0.7  
+        this.hull = 20;
+        this.firepower = 5;
+        this.accuracy = 0.7;
     }
-}
-
-
-attack(target); {
-    console.log("Captain Chaka Khan attacks with a vengance!");
-    let hitChance = Math.random();         
-    if (hitChance < this.accuracy) {
-        console.log(`It's a hit! The purse takes ${this.firepower} damage.`);
-        target.hull -= this.firepower; // 
-    } else {
-        console.log("The attack missed!");
-    }
-}
-
-Module.exports = CaptainChakaKhan
-
-
-
-class EnemyPurse {
-    constructor(hull, firepower, accuracy) {
-        this.hull = hull;
-        this.firepower = firepower; 
-        this.accuracy = accuracy; 
-    }
-
 
     attack(target) {
-        console.log("Oh nooo! The alien purse attacks slingshotting fireballs with its strap!");
-        let hitChance = Math.random();        
+        console.log("Captain Chaka Khan attacks with a couponers' vengeance!");
+        let hitChance = Math.random();
         if (hitChance < this.accuracy) {
-            console.log(`It's a hit! Captain Chaka Khan takes the designer ${this.firepower} damage to the forehead.`);
-            target.hull -= this.firepower;        
+            console.log(`It's a slap! GucciGlorilla takes level ${this.firepower} damage to its' metal logo.`);
+            target.hull -= this.firepower;
         } else {
-            console.log("The attack missed! The designer purse ramps up for another cost paying attack");
+            console.log("The attack missed!");
         }
     }
 }
 
-module.exports = EnemyPurse;
+class GucciGlorilla {
+    constructor() {
+        this.hull = Math.floor(Math.random() * 4) + 3;
+        this.firepower = Math.floor(Math.random() * 3) + 2;
+        this.accuracy = Math.random() * 0.2 + 0.6;
+    }
 
-
-const CaptainChakaKhan = require('./CaptainChakaKhan');
-const EnemyPurse = require('./EnemyPurse');
-
-const nubia = new CaptainChakaKhan();
-
-const gucciRaider = new EnemyPurse(
-    Math.floor(Math.random() * 4) + 3,  
-    Math.floor(Math.random() * 3) + 2,      Math.random() * 0.2 + 0.6);
-
-
-while (nubia.hull > 0 && gucciRaider.hull > 0) {
-    nubia.attack(gucciRaider); 
-    if (gucciRaider.hull > 0) { 
-        gucciRaider.attack(nubia); 
+    attack(target) {
+        console.log("GucciGlorilla attacks with a slingshotting fireball!");
+        let hitChance = Math.random();
+        if (hitChance < this.accuracy) {
+            console.log("Oh nooo! The GucciGlorilla strikes by slingshotting fireballs with its strap!");
+            target.hull -= this.firepower;
+        } else {
+            console.log("Gucci Glorilla misses! Captain Chaka Khan ramps up for another strategic attack.");
+        }
     }
 }
 
+const captainChakaKhan = new CaptainChakaKhan();
+const gucciGlorilla = new GucciGlorilla();
 
-if (nubia.hull > 0) {
-    console.log("Captain Chaka Khan wins and sent that Gucci bag straight to a garage sale clearance table!");
-} else {
-    console.log("The Gucci Raider wins and Captain Chaka Khan has to pay with an arm and a leg!");
+while (captainChakaKhan.hull > 0 && gucciGlorilla.hull > 0) {
+    captainChakaKhan.attack(gucciGlorilla);
+    if (gucciGlorilla.hull > 0) {
+        gucciGlorilla.attack(captainChakaKhan);
+    }
 }
 
+if (captainChakaKhan.hull > 0) {
+    console.log("Captain Chaka Khan wins and sent that Gucci bag straight to a garage sale clearance table!");
+} else {
+    console.log("The Gucci Glorilla wins and Captain Chaka Khan has to pay with an arm and a leg!");
+}
 
+// class CardiBlast {
+//     constructor() {
+//         this.hull = 20;
+//         this.firepower = 5;
+//         this.accuracy = 0.7;
+//     }
 
+//     attack(target) {
+//         console.log("Cardi Blast attacks!");
+//         let hitChance = Math.random();
+//         if (hitChance < this.accuracy) {
+//             console.log(`It's a hit! The purse takes level ${this.firepower} damage to the inner pocket.`);
+//             target.hull -= this.firepower;
+//         } else {
+//             console.log("The attack missed!");
+//         }
+//     }
+// }
 
+// class LouisVuittonLatto {
+//     constructor() {
+//         this.hull = Math.floor(Math.random() * 3) + 3;
+//         this.firepower = Math.floor(Math.random() * 2) + 2;
+//         this.accuracy = Math.random() * 0.2 + 0.6;
+//     }
+
+//     attack(target) {
+//         console.log("The Louis Vuitton Latto attacks with designer precision!");
+//         let hitChance = Math.random();
+//         if (hitChance < this.accuracy) {
+//             console.log(`It's a hit! The target takes level ${this.firepower} damage.`);
+//             target.hull -= this.firepower;
+//         } else {
+//             console.log("The attack missed!");
+//         }
+//     }
+// }
+
+// module.exports = { CaptainChakaKhan, GucciGlorilla, CardiBlast, LouisVuittonLatto };
